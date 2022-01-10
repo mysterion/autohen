a565e5f54cf03a23e850a7bd03247785aed4d41c
Super Dodgy Profile <49054698+mysterion@users.noreply.github.com>
2022-01-11 00:07:47 +0530

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -49,14 +49,61 @@ generate_service() {
   echo 'After=network.target' >> $file
   echo '[Service]' >> $file
   echo "User=$USER" >> $file
+  echo "Group=www-data" >> $file
   echo "WorkingDirectory=$wd" >> $file
   echo "ExecStart=/bin/bash -c '\"$wd/start.sh\"'" >> $file
   echo "[Install]" >> $file
   echo "WantedBy=multi-user.target" >> $file
 }
 
+generate_nginx() {
+  echo "server {" >> $DOMAIN
+  echo "    listen 80;" >> $DOMAIN
+  echo "    server_name $DOMAIN www.$DOMAIN;" >> $DOMAIN
+  echo "    location / {" >> $DOMAIN
+  echo "        include proxy_params;" >> $DOMAIN
+  echo "        proxy_pass http://unix:$wd/nginx.sock;" >> $DOMAIN
+  echo "    }" >> $DOMAIN
+  echo "}" >> $DOMAIN
+}
+
+setup_service() {
+  if [ -f "/etc/systemd/system/$1" ]; then
+    sudo systemctl stop $1
+    sudo rm /etc/systemd/system/$1
+  fi
+  
+  sudo mv ./$1 /etc/systemd/system/
+  sudo systemctl daemon-reload
+  sudo systemctl start $1
+  sudo systemctl enable $1
+}
+
+
+setup_nginx() {
+  if [ -f "/etc/nginx/sites-available/$DOMAIN" ]; then
+    sudo rm /etc/nginx/sites-enabled/$DOMAIN
+    sudo rm /etc/nginx/sites-available/$DOMAIN
+  fi
+  sudo mv ./$DOMAIN /etc/nginx/sites-available/
+  sudo chmod 644 /etc/nginx/sites-available/$DOMAIN 
+  sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
+  sudo nginx -t
+  if [ $? -eq 0 ]; then
+    echo "successfully configured nginx"
+  else
+    echo "failed to configure nginx"
+    sudo rm /etc/nginx/sites-enabled/$DOMAIN
+    sudo rm /etc/nginx/sites-available/$DOMAIN
+  fi
+  sudo systemctl restart nginx
+  echo "enabling firewall"
+  sudo ufw allow 'Nginx Full'
+}
+
 start_depl () {
   cd $wd
+  . conf.cfg
   if [ -d ./venv ]; then
     rm -rf ./venv
     if [ $? -ne 0 ]; then
@@ -68,7 +115,10 @@ start_depl () {
   source ./venv/bin/activate
   pip install -r requirements.txt
   chmod +x ./start.sh
-  generate_service ps4-exploit.service
+  generate_service ps4.service
+  setup_service ps4.service
+  generate_nginx
+  setup_nginx
 }
 
 main
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 wd=$(cd `dirname $0` && pwd)
 cd $wd
 source ./venv/bin/activate
-gunicorn -w 1 --bind unix:nginx.sock -m 007 wsgi:app
+gunicorn -w 1 --bind unix:nginx.sock -m 007 wsgi:app --log-level debug
